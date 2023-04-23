f257515bae8dcf7efa507b3155cf4fd1a9105ec9
danieskka <129956074+danieskka@users.noreply.github.com>
2023-04-23 21:58:45 +0200

    Add files via upload

---
+++ fundamentos-de-programacion-bikram-var-bucles-condicionales/index.js
@@ -0,0 +1,57 @@
+let variableSinValor;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = (PI + PI);
+var booleanoAnd = (booleano1 == booleano2);
+var booleanoNot = !booleano1;
+var booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1, !booleano2));
+var incrementarDesp = 2;
+var resultadoDesp = incrementarDesp++;
+var incrementarAntes = 2;
+var resultadoAntes = ++incrementarAntes;
+for (contarHasta10_2 = 0; contarHasta10_2 < 10; contarHasta10_2++){}
+
+var postI = 0;
+var postJ = 0;
+for(i = 0; i < 11; i++){
+    postI = postJ + postI;
+    postJ++;
+}
+
+
+var sumaPares = 0;
+for (i = 0; i < 10; i++){ 
+    if (i % 2 === 0) {
+        sumaPares = sumaPares + i;
+    }
+}
+let variableValorNumerico = 5;
+const MiNombre = "Daniel";
+const MiNumeroFav = 5;
+var booleanoOr = (booleano1 || booleano2) == true;
+var booleanoMix1 = (booleano1 && TAU/2 == PI) || (variableValorNumerico >= MiNumeroFav);
+var seisNoEsNueve = 6 != 9;
+var booleanoMix2 = (variableValorNumerico > 0 || variableValorNumerico <= -(MiNumeroFav * TAU));
+var valorSuma = MiNumeroFav + variableValorNumerico;
+var valorResta = MiNumeroFav - variableValorNumerico;
+var valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+var valorDivision = MiNumeroFav / 3;
+
+var contarHasta10 = 0;
+while(contarHasta10 < 10){
+    contarHasta10++;
+}
+
+var preI = 0;
+var preJ = 0;
+for(i = 0; i < 11; i++){
+    preI += ++preJ;
+}
+
+var sumaImpares = 0;
+for(i = 0; i < 10; i++){
+    if(i % 2 !== 0){
+        sumaImpares = sumaImpares + i;
+    }
+}
